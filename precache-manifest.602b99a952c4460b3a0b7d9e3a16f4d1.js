ae8d327344630d2dfd747cf93f7975fd89a3abd3
1Spaceone <dotss87@gmail.com>
2022-02-11 20:31:08 +0200

    Updates

---
+++ precache-manifest.602b99a952c4460b3a0b7d9e3a16f4d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a622d4688b4395ec5681f0b83f5c460e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9cb65d9e57760e0d7c5b",
+    "url": "./static/css/2.4b60f7c3.chunk.css"
+  },
+  {
+    "revision": "50f191e583e4f26ae4f6",
+    "url": "./static/css/main.02c749a2.chunk.css"
+  },
+  {
+    "revision": "9cb65d9e57760e0d7c5b",
+    "url": "./static/js/2.3c89aae9.chunk.js"
+  },
+  {
+    "revision": "1a29f8fb7f6a758d9dce8a0780f3b302",
+    "url": "./static/js/2.3c89aae9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50f191e583e4f26ae4f6",
+    "url": "./static/js/main.d87613a9.chunk.js"
+  },
+  {
+    "revision": "d852897cea0a694b3726",
+    "url": "./static/js/runtime-main.c06bb91b.js"
+  }
+]);
