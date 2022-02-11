9fc1eb1d926e8c1227ffc86435631a86da8b8f54
1Spaceone <dotss87@gmail.com>
2022-02-11 21:38:44 +0200

    master

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ function App() {
     <div className="App">
       <div className="Banner">
         <div className="Banner-description">
-          1Swap is unaudited software. Use at your own risk.
+          Onespace swap systems.
         </div>
       </div>
       <Routes />
       <div className="social-buttons">
         <GitHubButton
-          href="https://github.com/project-serum/oyster-swap"
+          href="https://1space.me/"
           data-color-scheme="no-preference: light; light: light; dark: light;"
           data-icon="octicon-star"
           data-size="large"
@@ -24,7 +24,7 @@ function App() {
           Star
         </GitHubButton>
         <GitHubButton
-          href="https://github.com/project-serum/oyster-swap/fork"
+          href="https://1space.me/"
           data-color-scheme="no-preference: light; light: light; dark: light;"
           data-size="large"
           aria-label="Fork project-serum/oyster-swap on GitHub"
